5b5714e4aa507d51d72214c37fc15dee63287ecf
rizalabimanyu2101 <plasticbomb202@gmail.com>
2025-03-10 02:24:30 +0700

    typo bos

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import Heading from "./Heading";
 import Footer from "./Footer";
-import BnnCoreContent from "./bnnCoreContent";
+import BnnCoreContent from "./BnnCoreContent";
 
 const App = () => {
-    return(
-        <div>
-            <Heading/>
-            <BnnCoreContent/>
-            <Footer/>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Heading />
+      <BnnCoreContent />
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
